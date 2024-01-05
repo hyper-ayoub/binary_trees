87a17351ff088ba8009dfd55396c2968a9a9980e
hyperayoub <bouagnaayoub@gmail.com>
2024-01-05 13:11:40 -0800

    binary_trees_project

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
