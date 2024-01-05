930a348f45e680581aea582cb0f22ecc0727a9d9
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-05 12:59:31 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -26,7 +26,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/*all functions*/
+/* all functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -68,4 +68,4 @@ int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 /* binary _tree _print utilities */
 void binary_tree_print(const binary_tree_t *);
-#endif 
+#endif
